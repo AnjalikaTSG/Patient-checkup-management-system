326ecff9644ce8de72bd5aaa8b8daa37b889044e
Gihani <gihaanjali@gmail.com>
2024-11-07 23:48:05 +0530

    Login Screen completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import RegisterScreen from './Pages/RegisterScreen';
+import LoginScreen from './Pages/LoginScreen';
 
 function App() {
   return (
     <div>
-      <RegisterScreen />
+      {/* <RegisterScreen /> */}
+      <LoginScreen/>
     </div>
   );
 }
---
+++ src/Pages/LoginScreen.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import '../Pages/RegisterScreen.css'; // Include your CSS styles
+
+function LoginScreen() {
+  return (
+    <div className="register-container">
+    <h3>Patient checkup management system - Base Hospital Avissawella</h3>
+    <div className="container">
+      <div className="form-container">
+        <h2>Login</h2>
+        <form>
+          <label>
+            Username:
+            <input type="text" name="username" />
+          </label>
+          <label>
+            Password:
+            <input type="password" name="password" />
+          </label>
+          <p>
+            Don't have an account? <a href="/login">Register</a>
+          </p>
+          <button type="submit" className="register-button">Login</button>
+        </form>
+      </div>
+      <div className="image-container">
+        <img src="/src/images/login2.jpg" alt="Medical Background" />
+      </div>
+    </div>
+    </div>
+  );
+}
+
+export default LoginScreen;
